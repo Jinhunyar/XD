e6a03a66f179f819c0e5ca210209b0cbcadf01be
Jinhunyar <119654992+Jinhunyar@users.noreply.github.com>
2023-01-10 08:24:37 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os, platform, time
+os.system('git pull')
+os.system('clear')
+print('follow my github and star my repo');time.sleep(3)
+os.system('xdg-open https://github.com/Ahmed-XD/insta')
+os.system('clear')
+import XNX
+XNX.makedirectory()
